0a804300dc28b82321b32dd88cfc42eb11ef7690
王圣智ST800197 <wangshengzhi@odc.cmbchina.cn>
2019-04-30 14:44:34 +0800

    init demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { PopupComponent } from './popup.component';
+import { PopupService } from './popup.service';
+
+// Include the `PopupService` provider,
+// but exclude `PopupComponent` from compilation,
+// because it will be added dynamically.
+
+@NgModule({
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [PopupService],
+  declarations: [AppComponent, PopupComponent],
+  bootstrap: [AppComponent],
+  entryComponents: [PopupComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {
+}
